215f455ad28589d0e9af9098cb4e2d00336e8c37
Akhmed <ahmed_@mail.ru>
2014-12-02 00:38:22 +0300

    Add project

---
+++ Logic/ExcelReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ClosedXML;
+using ClosedXML.Excel;
+
+namespace DemirPriceBalance.Logic
+{
+  class ExcelReader
+  {
+    public static void readExcel()
+    {
+      var xls = new XLWorkbook(@"C:\Users\hypnotic\Documents\DemirGroup\price\Юнипол.xlsx");
+      var wrs = xls.Worksheet("TDSheet");
+      var rowCnt = wrs.RowCount();
+      var res = wrs.Rows().Where(x => !String.IsNullOrEmpty(x.Cell(12).Value.CastTo<String>())).Select(x => new string[] { x.Cell(12).Value.CastTo<String>(), x.Cell(13).Value.CastTo<String>(), x.Cell(15).Value.CastTo<String>() });
+      res.Count();
+    }
+  }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using DemirPriceBalance.Logic;
+
+namespace DemirPriceBalance
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    private void btnMerge_Click(object sender, RoutedEventArgs e)
+    {
+      prbWork.IsIndeterminate = true;
+      var wrk = new BackgroundWorker();
+      wrk.DoWork += worker_DoWork;
+      wrk.ProgressChanged += worker_ProgressChanged;
+      wrk.RunWorkerCompleted += worker_RunWorkerCompleted;
+      wrk.RunWorkerAsync();
+    }
+
+    private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+    {
+      //throw new NotImplementedException();
+      prbWork.IsIndeterminate = false;
+    }
+
+    private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+    {
+      throw new NotImplementedException();
+    }
+
+    private void worker_DoWork(object sender, DoWorkEventArgs e)
+    {
+      ExcelReader.readExcel();
+      //throw new NotImplementedException();
+    }
+  }
+}
