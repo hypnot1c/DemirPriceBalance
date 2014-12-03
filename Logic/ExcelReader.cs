f850a31e99c502515acb9ca03aea40ca315443ed
hypnot1c <ahmed_@mail.ru>
2014-12-03 17:53:37 +0300

    working

--- Logic/ExcelReader.cs
+++ Logic/ExcelReader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.IO;
 
 using ClosedXML;
 using ClosedXML.Excel;
@@ -12,42 +13,60 @@ namespace DemirPriceBalance.Logic
 {
   class ExcelReader
   {
-    public static void readExcel()
+    public static Dictionary<string, string[]> readExcel(string file, Dictionary<string, object> parameters)
     {
-      using (var xls = new XLWorkbook(@"D:\Documents\Demir\Юнипол.xlsx"))
+      var _pageName = parameters["pageName"].ToString();
+      var _clmnIdInd = parameters["id"].CastTo<int>();
+      var _clmnPriceInd = parameters["price"].CastTo<int>();
+      var _clmnCntInd = parameters["count"].CastTo<int>();
+      using (var xls = new XLWorkbook(Path.GetFullPath(file)))
       {
-        var wrs = xls.Worksheet("TDSheet");
-        var rowCnt = wrs.RowCount();
-        var res = wrs.Rows().Where(x => !String.IsNullOrEmpty(x.Cell(12).Value.CastTo<String>()) && x.Cell(12).Value.CastTo<String>() != "Код производителя");
+        var wrs = xls.Worksheet(_pageName);
+
+        var res = wrs.Rows().Where(x => !String.IsNullOrEmpty(x.Cell(_clmnIdInd).Value.CastTo<String>()) && x.Cell(_clmnIdInd).Value.CastTo<String>() != "Код производителя");
         var goods = new Dictionary<string, string[]>(res.Count());
-        try
+        foreach (var i in res)
         {
-          foreach (var i in res)
+          var key = i.Cell(_clmnIdInd).RichText.Text;
+          if (!goods.ContainsKey(key))
+            goods.Add(key, new string[] { i.RowNumber().ToString(), i.Cell(_clmnPriceInd).RichText.Text, i.Cell(_clmnCntInd).RichText.Text });
+          else
+            Debug.WriteLine(key);
+        }
+        return goods;
+      }
+    }
+
+    public static void writeExcel(string file, Dictionary<string, string[]> data)
+    {
+      using (var xls = new XLWorkbook(Path.GetFullPath(file)))
+      {
+        var sheets = new Dictionary<string, Dictionary<string, object>> { {"Шины", new Dictionary<string, object> { { "price", (object)"X" } } } };
+        using (var wrs = xls.Worksheet("Шины"))
+        {
+          var rows = wrs.Rows().Where(x => !String.IsNullOrEmpty(x.Cell("A").Value.ToString()));
+          var goods = new Dictionary<string, int>(wrs.RowCount());
+          foreach (var i in rows)
           {
-            var key = i.Cell(12).RichText.Text;
+            var key = i.Cell("A").RichText.Text;
             if (!goods.ContainsKey(key))
-              goods.Add(key, new string[] { i.RowNumber().ToString(), i.Cell(13).RichText.Text, i.Cell(15).RichText.Text });
+              goods.Add(key, i.RowNumber());
             else
-              Debug.WriteLine(key);
+              Debug.WriteLine("Result: " + key);
+          }
+          foreach (var i in goods)
+          {
+            if (data.ContainsKey(i.Key))
+            {
+              var vls = data[i.Key];
+              wrs.Cell(i.Value, "X").Value = vls[1];
+              int count = 0;
+              var isNum = Int32.TryParse(vls[2], out count);
+              wrs.Cell(i.Value, "W").Value = isNum ? (object)count : (object)String.Empty;
+            }
           }
         }
-        catch(Exception ex)
-        {
-          ex.ToString();
-        }
-        //res.Count();
-
-        using(var xls2 = new XLWorkbook(@"D:\Documents\Demir\DEMIR шины и диски 20.10.2014.xlsx"))
-        {
-          wrs = xls2.Worksheet("Шины");
-          var t = wrs.Row(4).Cell(16).Value;
-          var inr = wrs.Rows().Select(x => x.Cell(1).Value.CastTo<String>()).Intersect(goods.Keys);
-        }
-        using (var xls3 = new XLWorkbook(@"D:\Documents\Demir\v8_2BA4_378.xlsx"))
-        {
-          wrs = xls3.Worksheet("TDSheet");
-          var inr = wrs.Rows().Select(x => x.Cell(1).Value.CastTo<String>()).Intersect(goods.Keys);
-        }
+        xls.SaveAs(@"D:\Documents\GitHub\DemirPriceBalance\docs\DEMIR шины и диски 20.10.20141.xlsx");
       }
     }
   }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,30 +13,37 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+//using System.Windows.Shapes;
 
 using DemirPriceBalance.Logic;
+using Microsoft.Win32;
+using System.Diagnostics;
 
 namespace DemirPriceBalance
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
+      txtDemirTires.Text = Path.GetFullPath(Properties.Resources.ResourceManager.GetString("demirTiresFile"));
+      txtUnipol.Text = Path.GetFullPath(Properties.Resources.ResourceManager.GetString("unipolFile"));
+      txtShinService.Text = Path.GetFullPath(Properties.Resources.ResourceManager.GetString("shinServiceFile"));
+      txtSaRu.Text = Path.GetFullPath(Properties.Resources.ResourceManager.GetString("saRuFile"));
+    }
 
     private void btnMerge_Click(object sender, RoutedEventArgs e)
     {
       prbWork.IsIndeterminate = true;
       var wrk = new BackgroundWorker();
+      wrk.WorkerReportsProgress = true;
       wrk.DoWork += worker_DoWork;
       wrk.ProgressChanged += worker_ProgressChanged;
       wrk.RunWorkerCompleted += worker_RunWorkerCompleted;
-      wrk.RunWorkerAsync();
+      wrk.RunWorkerAsync(new string[] { txtUnipol.Text, txtShinService.Text, txtSaRu.Text, txtDemirTires.Text });
     }
 
     private void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
@@ -46,13 +54,83 @@ namespace DemirPriceBalance
 
     private void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
     {
-      throw new NotImplementedException();
+      switch(e.ProgressPercentage)
+      {
+        case 1:
+          lblState.Content = "Reading Unipol file...";
+          break;
+        case 2:
+          lblState.Content = "Reading ShinService file...";
+          break;
+        case 3:
+          lblState.Content = "Reading SaRu file...";
+          break;
+        case 4:
+          lblState.Content = "Merging and saving result...";
+          break;
+        default:
+          lblState.Content = "Done.";
+          break;
+      }
     }
 
     private void worker_DoWork(object sender, DoWorkEventArgs e)
     {
-      ExcelReader.readExcel();
-      //throw new NotImplementedException();
+      var wrk = (BackgroundWorker)sender;
+      wrk.ReportProgress(1);
+      var args = (string[])e.Argument;
+      var parameters = new Dictionary<string, object> { { "pageName", "TDSheet" }, { "id", 12 }, { "price", 13 }, { "count", 15 } };
+      var uni = ExcelReader.readExcel(args[0], parameters);
+      wrk.ReportProgress(2);
+      parameters["id"] = 1;
+      parameters["price"] = 8;
+      parameters["count"] = 9;
+      var shin = ExcelReader.readExcel(args[1], parameters);
+      wrk.ReportProgress(3);
+      parameters["pageName"] = "Легковая резина";
+      parameters["id"] = 17;
+      parameters["price"] = 6;
+      parameters["count"] = 3;
+      var sa = ExcelReader.readExcel(args[2], parameters);
+      wrk.ReportProgress(4);
+      try
+      {
+        ExcelReader.writeExcel(args[3], uni);
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show("Error saving file", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+      }
+      wrk.ReportProgress(5);
+      GC.Collect();
+    }
+
+
+    private void btnSelect_Click(object sender, RoutedEventArgs e)
+    {
+      var btnSrc = (Button)sender;
+      var dlg = new OpenFileDialog();
+      dlg.DefaultExt = "xlsx";
+      dlg.Filter = "Excel workbook|*.xlsx";
+      var res = dlg.ShowDialog();
+      if (res.HasValue && res.Value)
+      {
+        switch(btnSrc.Name)
+        {
+          case "txtDemirTires":
+            txtDemirTires.Text = dlg.FileName;
+            break;
+          case "txtShinService":
+            txtShinService.Text = dlg.FileName;
+            break;
+          case "txtUnipol":
+            txtUnipol.Text = dlg.FileName;
+            break;
+          case "txtSaRu":
+            txtSaRu.Text = dlg.FileName;
+            break;
+        }
+      }
     }
   }
 }
