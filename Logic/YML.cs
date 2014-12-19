f086f5d06a25adf228c7bfb5e5cd1ac879ed949e
hypnot1c <ahmed_@mail.ru>
2014-12-19 17:54:26 +0300

    Coding YML generator

--- Logic/YML.cs
+++ Logic/YML.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Xml.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace DemirPriceBalance.Logic
 {
   class YML
   {
-    public YML()
+    public YML(string filePath)
     {
-      ;
+      using (var xml = XmlReader.Create(filePath))
+      {
+        this.PriceList = XDocument.Load(xml);
+      }
     }
+
+    public XDocument PriceList
+    {
+      get;
+      set;
+    }
+
+
   }
 }
